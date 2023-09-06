bb3bc471e1632ec4cab5b5b5a6ac279f344f0dd5
Himanshu Sah <himanshusah41@gmail.com>
2023-09-05 20:28:03 -0400

    Server added

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// 
+
+//          Server Starts Here
+
+// 
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+dotenv.config();
+
+// Establish and confirm mongoose connection 
+mongoose.connect(process.env.ATLAS_URI,{useNewUrlParser:true,useUnifiedTopology:true}). 
+then(()=>{
+    console.log("Connected to the database")
+}).catch(error=>{
+    console.log("Error: ",error);
+})
+
+//middleware
+app.use(express.json());
+
+app.listen(PORT,() => {
+    console.log(`E-commerce site listening on ${PORT}`);
+})
