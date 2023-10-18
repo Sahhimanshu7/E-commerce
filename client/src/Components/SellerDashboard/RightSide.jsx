a33e5c6d33cd68c156cbfa8fe1b8fb7b883ad807
Himanshu Sah <himanshusah41@gmail.com>
2023-10-17 20:10:56 -0400

    Image upload successfull

--- client/src/Components/SellerDashboard/RightSide.jsx
+++ client/src/Components/SellerDashboard/RightSide.jsx
@@ -1,8 +1,48 @@
+import axios from 'axios';
+import {  useState } from 'react';
+import { v4 } from "uuid";
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+  } from "firebase/storage";
+import { storage } from '../../firebase';
+
+  
+
 export const About = ({id}) =>{
+    const [imageUpload, setImageUpload] = useState(null);
+    const [imageUrls, setImageUrls] = useState(null);
+
+    // const imagesListRef = ref(storage, "images/");
+    const uploadFile = async() => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls(url);
+      });
+    });
+    await axios.put('/api/seller/uploadProfileImg/',{
+      sellerID:id,
+      profileImage: imageUrls
+    }).then((res) => console.log(res))
+    .catch((error)=>{ console.log(error)});
+  };
+
+  
+ 
     return(
         <div className="About">
             <div className="image">
-                
+            <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+      
             </div>
         </div>
     )
---
+++ client/src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBcn3BeVFIi21zuRoGwek-Uj90mIz5q548",
+  authDomain: "e-c999.firebaseapp.com",
+  projectId: "e-c999",
+  storageBucket: "e-c999.appspot.com",
+  messagingSenderId: "793313012133",
+  appId: "1:793313012133:web:958552fb09fdab4223a04c",
+  measurementId: "G-4049KZC8NR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const storage = getStorage(app);
