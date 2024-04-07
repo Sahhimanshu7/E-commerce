00ea25406af37054bba34c3f104f762bad230aa6
Himanshu Sah <himanshusah41@gmail.com>
2024-04-07 12:26:20 -0400

    Showing Products

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -22,20 +22,44 @@ const Home = () => {
         console.log(error);
       }
     }
-    getProducts();
+    getProducts();    
   },[])
-  
+
   console.log(products);
   return (
     <div>
       {
         products.length > 0 ? (
-          <div >
+          <div className="">
             {products.map((category, index) => (
-              <div key={index}>
+              <div key={index} className="flex flex-wrap ml-2">
                 {category.map((prod, keys) => (
-                  <div key={keys}>
-                    {prod.productName}
+                  <div key={keys} className="flex">
+                    {!prod.productName && (
+                      <div className="w-[100vw] text-gray-600 md:pl-0 pl-8">
+                        {prod}
+                        <br />
+                      </div>
+                    )
+                    }
+                    
+                    {prod.productName && (
+                      <div className=""> 
+                        <div className="border p-2 w-[210px] m-2 rounded-lg bg-slate-200 shadow-xl md:ml-0 ml-20">
+                          <div>
+                            <img src={`${PORT}/api/products/show-image/${prod.photoURL}`} alt="Product Image" 
+                            className="w-[180px] h-[180px] rounded-lg m-1"
+                            />
+                          </div>
+                          <div>
+                            <p>{prod.productName}</p>
+                            <p>{prod.finalPrice}</p>
+                            <p>{prod.brand}</p>
+                            <p>{prod.sellerName}</p>
+                          </div>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 ))}
               </div>
@@ -52,8 +76,3 @@ const Home = () => {
 }
 
 export default Home
-
-// products.length > 0 && 
-//         products[0].map((p) => {
-//           console.log(p);
-//         })
